f9fc928427173fe54d13b44ab69928943e30b50a
Ivan <OtradinskiyIvan>
2026-05-30 23:29:49 +0300

    feat: add username param to User entity

--- authorization_service/src/domain/entities/user.py
+++ authorization_service/src/domain/entities/user.py
@@ -6,6 +6,7 @@ from uuid import UUID
 @dataclass(frozen=True)
 class User:
     id: UUID
+    username: str
     email: str
     hashed_password: str
     is_active: bool = True
@@ -15,6 +16,7 @@ class User:
     def deactivate(self) -> "User":
         return User(
             id=self.id,
+            username=self.username,
             email=self.email,
             hashed_password=self.hashed_password,
             is_active=False,
