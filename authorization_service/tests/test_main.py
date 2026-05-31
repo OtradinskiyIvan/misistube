be607aab92c690e87dc3b916486c6ba591c7c65d
Ivan <OtradinskiyIvan>
2026-05-31 16:10:15 +0300

    fix: new unittests, coverage 80%

---
+++ authorization_service/tests/test_main.py
@@ -0,0 +1,16 @@
+# tests/test_main.py
+import unittest
+from unittest.mock import patch, MagicMock
+
+
+class TestMainApp(unittest.TestCase):
+    
+    @patch('shared.database.session.init_engine')
+    @patch('shared.database.session.Base', new_callable=MagicMock)
+    def test_app_imports_and_routes(self, mock_base, mock_init_engine):
+        """Тест без реального подключения к БД"""
+        from authorization_service.src.main import app
+        
+        self.assertTrue(hasattr(app, "title"))
+        routes = [route.path for route in app.routes]
+        self.assertTrue(any(r.startswith('/api/v1/auth') for r in routes))
