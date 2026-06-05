116250a6048a8f357a0b69fb6fc75c91d0e1851b
Kirill <kirillbestugev@gmail.com>
2026-06-05 20:45:52 +0300

    chore: update ports for player_search service and add .env.example for creation_search couple

--- frontend/player_search_service/vite.config.js
+++ frontend/player_search_service/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // твой backend
+        target: 'http://localhost:8001', // твой backend
         changeOrigin: true,
       }
     }
