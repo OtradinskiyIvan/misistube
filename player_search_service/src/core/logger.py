7d74417ec38fc867ee6fe51576a6427d70210319
Ivan <OtradinskiyIvan>
2026-06-07 21:37:14 +0300

    fix: fixed root dir parents ([3] -> [2])

--- player_search_service/src/core/logger.py
+++ player_search_service/src/core/logger.py
@@ -3,7 +3,7 @@ import sys
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
-ROOT_DIRECTORY = Path(__file__).resolve().parents[3]
+ROOT_DIRECTORY = Path(__file__).resolve().parents[2]
 if str(ROOT_DIRECTORY) not in sys.path:
     sys.path.insert(0, str(ROOT_DIRECTORY))
 
