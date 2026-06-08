63c43438b9feb0a4b3075f68f0eb84072f209781
Ivan <OtradinskiyIvan>
2026-06-08 21:00:03 +0300

    feat: add vite interaction api target to config.js

--- frontend/player_search_service/vite.config.js
+++ frontend/player_search_service/vite.config.js
@@ -3,12 +3,17 @@ import React from '@vitejs/plugin-react'
 
 const PLAYER_API_TARGET = process.env.VITE_PLAYER_API_TARGET || `http://localhost:8004`
 const USER_API_TARGET = process.env.VITE_USER_API_TARGET || `http://localhost:8000`
+const INTERACTION_API_TARGET = process.env.VITE_INTERACTION_API_TARGET || 'http://localhost:8002'
 
 export default defineConfig({
   plugins: [React()],
   server: {
     port: 5173,
     proxy: {
+      '/api/v1/comments': {
+        target: INTERACTION_API_TARGET,
+        changeOrigin: true,
+      },
       '/api/v1/users': {
         target: USER_API_TARGET,
         changeOrigin: true,
